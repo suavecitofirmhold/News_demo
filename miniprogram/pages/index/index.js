7c2300656ea61f6d9df8a9ae93e027d9b0910427
suavecitofirmhold <zackchen5561@gmail.com>
2019-02-13 12:59:25 +0800

    fifth

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,19 +1,40 @@
 //index.js
 //const app = getApp()
+const newsCategory = [
+  { id: 'gn', name: '国内' },
+  { id: 'gj', name: '国际' },
+  { id: 'cj', name: '财经' },
+  { id: 'yl', name: '娱乐' },
+  { id: 'js', name: '纪实' },
+  { id: 'ty', name: '体育' },
+  { id: 'other', name: '综合' }
+]
+
+const currCate = [
+  { ind: 0, id: 'gn'},
+  { ind: 1, id: 'gj' },
+  { ind: 2, id: 'cj' },
+  { ind: 3, id: 'yl' },
+  { ind: 4, id: 'js' },
+  { ind: 5, id: 'ty' },
+  { ind: 6, id: 'other' },
+]
 
 Page({
   data: {
-    allkinds: ["国内","国际","财经","娱乐","军事","体育","其他"]
+    newsCategory,  //新闻分类
+    current: 0,   //当前分类
   },
   onPullDownRefresh(){
     this.getNews(()=>{
       wx.stopPullDownRefresh()
     })
+    console.log("stopPulldown")
   },
   onLoad(){
-    this.getNews()
+    this.getNews(0)
   },
-  getNews(callback){
+  getNews(callback, index){
     wx.request({
       url: 'https://test-miniprogram.com/api/news/list', 
       data: {
@@ -36,8 +57,6 @@ Page({
             //result[i].firstImage
           })
         }
-        console.log(firstImage)
-
         this.setData({
           news: news
         })
@@ -47,9 +66,37 @@ Page({
       }
     })
   },
-  onTapList(){
-    wx.showToast()
-  },
+  handleCateChange: function(e){
+    let index = e.currentTarget.dataset.index;
+    console.log(currCate[index].id)
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list',
+      data: {
+        type: currCate[index].id
+      },
+      success: res => {
+        console.log(res.data)
+        let result = res.data.result
+        let title = result[0].title
+        let date = result[0].date
+        let firstImage = result[0].firstImage
+        //console.log(title, date)
+        let news = []
+        for (let i = 0; i < result.length; i++) {
+          news.push({
+            title: result[i].title,
+            date: result[i].date.substring(0, 10),
+            firstImage: result[i].firstImage,
+            // iconPath: '/images/code-db-inc-dec.png'  
+            //result[i].firstImage
+          })
+        }
+        this.setData({
+          news: news
+        })
+      }
+    })
+    },
   onTapDetail(){
     wx.showToast()
   }
